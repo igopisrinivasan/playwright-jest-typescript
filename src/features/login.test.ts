095ecce6b791d81f8ab441d7005d5102b7251432
Gopi Srinivasan <igopisrinivasan@gmail.com>
2021-02-23 00:08:52 +0530

    video recording added

--- src/features/login.test.ts
+++ src/features/login.test.ts
@@ -3,15 +3,16 @@ describe('Launch Browser', () => {
 
     test('Open Orange HRM', async () => {
         const browser = await chromium.launch({
-            headless: false
+            headless: true
         })
-        const context = await browser.newContext();
+        const context = await browser.newContext({ recordVideo: { dir: 'results/videos/',size: { width: 1920, height: 1080 } } });
         const page = await context.newPage();
         await page.goto('https://opensource-demo.orangehrmlive.com/');
         await page.fill("input[id='txtUsername']", 'Admin');
         await page.fill("input[id='txtPassword']", 'admin123');
         await page.click("input[id='btnLogin']")
         await browser.close()
+        //await context.close()
     })
 
 })
